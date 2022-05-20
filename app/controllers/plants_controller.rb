8123140c1cf31a9c6700dc2941626bdcd289b0f6
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-19 22:01:09 -0400

    LESSON COMPLETE FINALLY

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,8 +6,17 @@ class PlantsController < ApplicationController
     end
 
     def show
+        plant = Plant.find_by(id: params[:id])
+        render json: plant
     end
 
     def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+    def plant_params
+        params.permit(:name, :image, :price)
     end
 end
